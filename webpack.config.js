fb0af32bb2b45391abeba070bf941ba49d1e8977
Jay Shah <jayyshah69@gmail.com>
2025-03-03 19:30:18 -0500

    Fixed the ability to work with images and Created html for header and footer for the page

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,11 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        // HTML LOADER
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
     ],
   },
 };
